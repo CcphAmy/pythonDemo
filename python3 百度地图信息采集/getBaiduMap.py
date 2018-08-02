3f98d89367eac93164cb436bf37d0a51e0f39c6c
陈沛杭 <wojiaopeihang@vip.qq.com>
2018-08-02 22:18:57 +0800

    python3 百度地图信息采集(未完成)
    python3 百度地图信息采集(未完成)

--- python3 百度地图信息采集/getBaiduMap.py
+++ python3 百度地图信息采集/getBaiduMap.py
@@ -8,23 +8,30 @@ class BaiduMap(object):
 	def __init__(self):
 		super(BaiduMap, self).__init__()
 
-
 	#城市获取数据
 	def getCityData(cityName):
 		# http://map.baidu.com/?newmap=1&qt=cur&ie=utf-8&wd=  &oue=1&res=jc
 		try:
 			webData = requests.get("http://map.baidu.com/?newmap=1&qt=cur&ie=utf-8&wd=" + cityName + "&oue=1&res=jc").text
 			jsonData = json.loads(webData)
-			# print(jsonData)
+			print(jsonData,end="\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
 
-			if 'weather' in jsonData : #存在天气预报的情况下
+
+			if 'weather' in jsonData: #存在天气预报的情况下
 				weatherData = json.loads(jsonData['weather'])
-				print(weatherData['OriginQuery']," PM2.5:",weatherData['pm25'],weatherData['weather0'],"[",weatherData['temp0'],"][",weatherData['wind0'],"]")
+				print(weatherData['OriginQuery']," PM2.5:",weatherData['pm25'],weatherData['weather0'],"[",weatherData['temp0'],"][",weatherData['wind0'],"]",end=' ')
 
+			if 'cur_area_id' in jsonData:
+				print("城市id:",jsonData['cur_area_id'])
+				return jsonData['cur_area_id']
+			else:
+				return -1
 
 		except Exception as e:
 			raise
 
+	def getMapData(cityId,info_):
+		pass
 
 if __name__ == '__main__':
-	BaiduMap.getCityData("广州");
+	BaiduMap.getCityData("潮州");
